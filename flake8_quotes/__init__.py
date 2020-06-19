4e170ab78f667616586f266f1503a83312688dbd
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-06-19 22:05:36 +0100

    Trying to debug

--- flake8_quotes/__init__.py
+++ flake8_quotes/__init__.py
@@ -276,6 +276,7 @@ class QuoteChecker(object):
 
                 # If not preferred type, only allow use to avoid escapes.
                 if not self.config['good_single'] in string_contents:
+                    print(string_contents, unprefixed_string)
                     if not (len(string_contents) <= 1 and re.match(r"^'.?'$", unprefixed_string)):
                         yield {
                             'message': 'Q000 Remove bad quotes',
